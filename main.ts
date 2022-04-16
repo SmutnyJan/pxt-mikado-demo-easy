904d1ebebbb1483981caa14df1b0a2c78a16f644
SmutnyJan <57211415+SmutnyJan@users.noreply.github.com>
2022-04-16 16:28:46 +0200

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,47 @@
-
+let jeZapnutoHlidani = false
+let jeProvadenaAkce = false
+let stavDispleje = ""
+mikado.onGuardAwaken(Difficulty.Easy, function () {
+    if (jeZapnutoHlidani == true) {
+        jeProvadenaAkce = true
+        stavDispleje = "nastvany"
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . . . .
+            . # # # .
+            # . . . #
+            `)
+        soundExpression.sad.playUntilDone()
+        jeProvadenaAkce = false
+    }
+})
+input.onButtonPressed(Button.A, function () {
+    if (jeZapnutoHlidani == false) {
+        jeZapnutoHlidani = true
+    } else {
+        jeZapnutoHlidani = false
+    }
+})
+basic.forever(function () {
+    serial.writeLine(stavDispleje)
+    if (!(jeZapnutoHlidani) && stavDispleje != "vesely") {
+        stavDispleje = "vesely"
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . . . . .
+            # . . . #
+            . # # # .
+            `)
+    } else if (jeZapnutoHlidani && !(jeProvadenaAkce) && stavDispleje != "pozor") {
+        stavDispleje = "pozor"
+        basic.showLeds(`
+            # # . # #
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+    }
+})
